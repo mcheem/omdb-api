99a99fbbc240669bb31b63612480452fc0ff4850
mcheem <43473642+mcheem@users.noreply.github.com>
2019-11-22 14:09:45 +0000

    v0.3
    added info URL

--- app.py
+++ app.py
@@ -21,6 +21,31 @@ def movie():
     return render_template('movie.html', items=items)
 
 
+@app.route('/info/<id>', methods=['POST','GET'])
+def info(id):
+    apikey = '6f6c977'
+    imdb_search = id
+    r = requests.get('http://www.omdbapi.com/?apikey='+apikey+'&i='+imdb_search)
+    json_object = r.json()
+
+    title = json_object['Title']
+    rated = json_object['Rated']
+    poster = json_object['Poster']
+    runtime = json_object['Runtime']
+    plot = json_object['Plot']
+    '''
+    items = json_object['Search']
+
+    for item in items:
+        title = item['Title']
+        year = item['Year']
+        poster = item['Poster']
+        imdbID = item['imdbID']
+    '''
+    #return json_object
+    return render_template('info.html', title=title, poster=poster, rated=rated, runtime=runtime, plot=plot)
+
+
 @app.route('/')
 def index():
 	return render_template('index.html')
